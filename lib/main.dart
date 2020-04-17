3fbccc8c2e0782ffa2e299d36f93e339c07bcbed
Marco Mercad <mmerc007@ucr.edu>
2020-04-17 00:06:03 -0700

    update verions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart' as L;
@@ -8,6 +9,7 @@ import 'dart:math';
 void main() => runApp(MyApp());
 
 
+enum WhyFather { harder, smarter, selfStarter, tradingCenter}
 
 class MyApp extends StatelessWidget {
   @override
@@ -30,20 +32,44 @@ class MyMapState extends State<MapSample> {
   GoogleMapController mapController;
   var location = new L.Location();
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{}; // CLASS MEMBER, MAP OF MARKS
-  var markersID = 0;
-  LatLng myLocation =  LatLng(37.4219983,-122.084);
-  TextEditingController _distance = new TextEditingController();
-  TextEditingController _endPoint = new TextEditingController();
+  bool typing = false;
+  static TextEditingController _distance = new TextEditingController();
+  TextBox distance = new TextBox(_distance);
+
+
+  static LatLng myLocation =  LatLng(39.8283,-98.5795);
+
 
   Set<Polyline> polyline = {};
   List<LatLng> routeCoords;
-
+  List<bool> isSelected;
 
   @override
   void initState() {
     // TODO: implement initState
+    isSelected = [true, false];
+    _getLocation();
     super.initState();
+
+
   }
+
+  void setInitialLocation() async {
+    try {
+      await location.getLocation().then((onValue) {
+        myLocation = LatLng(onValue.latitude, onValue.longitude);
+        print(onValue.latitude.toString() + "," + onValue.longitude.toString());
+      });
+    } catch (e) {
+      print(e);
+      if (e.code == 'PERMISSION_DENIED') {
+        var askpermissions = await Permission.requestPermissions([PermissionName.Location]);
+      }
+    }
+
+  }
+
+
 
   void _onMapCreated(GoogleMapController controller) {
 
@@ -59,23 +85,27 @@ class MyMapState extends State<MapSample> {
 //          endCap: Cap.buttCap));
     }
     );
-    
+
 
   }
+
+
+
+
   void getSomePoints(range,origen, destination) async{
     var permissions = await Permission.getPermissionsStatus([PermissionName.Location]);
     if(permissions[0].permissionStatus == PermissionStatus.notAgain){
       var askpermissions = await Permission.requestPermissions([PermissionName.Location]);
     }else{
       List<LatLng> route;
-       await googleMapPolyline.getCoordinatesWithLocation(
+      await googleMapPolyline.getCoordinatesWithLocation(
           origin: origen,
           destination: destination,
-          mode: RouteMode.driving
-       ).then((onValue) {
-            print("the coordiates");
-            routeCoords= onValue;
-            print(routeCoords);
+          mode: RouteMode.walking
+      ).then((onValue) {
+        print("the coordiates");
+        routeCoords= onValue;
+        print(routeCoords);
       });
 
     }
@@ -98,6 +128,9 @@ class MyMapState extends State<MapSample> {
     double segment = totalMiles/4;
     double change_long =change_in_longitude(segment);
     double change_lat = change_in_latitude(segment);
+    print("change latitude " + change_lat.toString());
+    print("change long " + change_long.toString());
+
     for( var i = 0 ; i <= 4; i++ ) {
       if(i == 0){
         getSomePoints(i, myLocation, LatLng(myLocation.latitude,myLocation.longitude+change_long ));
@@ -108,9 +141,6 @@ class MyMapState extends State<MapSample> {
       }if(i == 4){
         getSomePoints(i, LatLng(myLocation.latitude+change_lat,myLocation.longitude ), myLocation);
       }
-
-
-      
     }
 
   }
@@ -134,29 +164,8 @@ class MyMapState extends State<MapSample> {
     try {
       await location.getLocation().then((onValue) {
         myLocation = LatLng(onValue.latitude, onValue.longitude);
-        mapController.animateCamera(CameraUpdate.newCameraPosition(
-          CameraPosition(
-            bearing: 0,
-            target: LatLng(onValue.latitude, onValue.longitude),
-            zoom: 17.0,
-          ),
-        ));
         print(onValue.latitude.toString() + "," + onValue.longitude.toString());
-        final MarkerId markerIdT = MarkerId(markersID.toString());
-        final Marker marker = Marker(
-          markerId: markerIdT,
-          position: LatLng(
-            onValue.latitude,
-            onValue.longitude,
-          ),
-          infoWindow: InfoWindow(title: "My Location", snippet: '*'),
-        );
-        setState(() {
-          markers[markerIdT] = marker;
-        });
-
       });
-
     } catch (e) {
       print(e);
       if (e.code == 'PERMISSION_DENIED') {
@@ -165,139 +174,151 @@ class MyMapState extends State<MapSample> {
     }
   }
 
+  void _pushSave(){
+    Navigator.of(context).push(
+        new MaterialPageRoute(builder: (context) {
+          return new Scaffold(
+              appBar: AppBar(
+                title: new Text("Settings"),
+
+              ),
+              body:
+
+              Column(
+                  children: <Widget>[
+
+                    ToggleButtons(
+                      borderColor: Colors.black,
+                      fillColor: Colors.grey,
+                      borderWidth: 2,
+                      selectedBorderColor: Colors.black,
+                      selectedColor: Colors.white,
+                      borderRadius: BorderRadius.circular(0),
+                      children: <Widget>[
+                        Text(
+                          'Open 24 Hours',
+                          style: TextStyle(fontSize: 16),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.all(1.0),
+                          child: Text(
+                            'Custom Hours',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
+                      onPressed: (int index) {
+                        setState(() {
+                          for (int i = 0; i < isSelected.length; i++) {
+                            isSelected[i] = i == index;
+                          }
+                        });
+                      },
+                      isSelected: isSelected,
+                    ),
 
 
 
+                  ])
+          );
+        })
+
+
+    );
+
+  }
+
+  void createPolyLines(miles){
+    print("creating the lines");
+    setPolyLines(3);
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home:
+        home:
 
-      Scaffold(
-          appBar: AppBar(
-            title: Text('Runout'),
-            backgroundColor: Colors.green[700],
-          ),
-          body:
-          Column(
-            children: <Widget>[
-              Text('Deliver features faster'),
-              Text('Craft beautiful UIs'),
-              Flexible(
-                flex: 2,
-                child:
-                GoogleMap(
-                  polylines: polyline,
-                  onMapCreated: _onMapCreated,
-                  initialCameraPosition: CameraPosition(
-                    target: myLocation,
-                    zoom: 11.0,
-                  ),
-                    markers: Set<Marker>.of(markers.values),
-                ),
+        Scaffold(
+//          appBar: AppBar(
+//            title: Text('Runout'),
+//            backgroundColor: Colors.green[700],
+//          ),
+            appBar: AppBar(
+              title: typing ? distance : Text("BikeOut"),
+              leading: IconButton(
+                icon: Icon(typing ? Icons.done : Icons.search),
+                onPressed: () {
+                  setState(() {
+                    polyline.clear();
+                    typing = !typing;
+                  });
+                  if(typing == false){
+                    setPolyLines( double.parse( _distance.text));
 
+                  }
+                  },
               ),
+              actions: <Widget>[
+                new IconButton(icon: Icon(Icons.settings), onPressed: _pushSave),
+              ],
+            ),
 
-              Flexible(
-                flex: 1,
-                child:
-                Row(children: <Widget>[
-                  Flexible(
-                      flex: 1,
-                      child: Container(
-                        child: Text('Enter range'),
-                      ),
-                  ),
-                  Flexible(
-                      flex: 4,
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                        child: TextField(
-                          controller: _distance ,
-                          obscureText: false,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Distance',
-                            ),
-                          ),
-                      ),
-                  ),
+            body:
 
-
-//                  Flexible(
-//                    flex: 2,
-//                    child: Container(
-//                      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-//                      child: TextField(
-//                        controller: _endPoint ,
-//                        obscureText: false,
-//                        decoration: InputDecoration(
-//                          border: OutlineInputBorder(),
-//                          labelText: 'End Point',
-//                        ),
-//                      ),
+            Column(
+                children: <Widget>[
+//
+//
+//              ToggleButtons(
+//                borderColor: Colors.black,
+//                fillColor: Colors.grey,
+//                borderWidth: 2,
+//                selectedBorderColor: Colors.black,
+//                selectedColor: Colors.white,
+//                borderRadius: BorderRadius.circular(0),
+//                children: <Widget>[
+//                  Text(
+//                    'Open 24 Hours',
+//                    style: TextStyle(fontSize: 16),
+//                  ),
+//
+//                  Padding(
+//                    padding: const EdgeInsets.all(1.0),
+//                    child: Text(
+//                      'Custom Hours',
+//                      style: TextStyle(fontSize: 16),
 //                    ),
 //                  ),
-                ]),
-          ),
+//                ],
+//                onPressed: (int index) {
+//                  setState(() {
+//                    for (int i = 0; i < isSelected.length; i++) {
+//                      isSelected[i] = i == index;
+//                    }
+//                  });
+//                },
+//                isSelected: isSelected,
+//              ),
 
-              Flexible(
-                child:
-                    Row(children: <Widget>[
 
-                  Container(
-                      margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  Flexible(
+                    child:
+                    GoogleMap(
+                        myLocationButtonEnabled: true,
+                        myLocationEnabled: true,
+                        polylines: polyline,
+                        onMapCreated: _onMapCreated,
+                        initialCameraPosition: CameraPosition(
+                          target: myLocation,
+                          zoom: 2.0,
+                        )
+                    ),
 
-                      child:
-                    FlatButton(
-
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      disabledColor: Colors.grey,
-                      disabledTextColor: Colors.black,
-                      padding: EdgeInsets.all(8.0),
-                      splashColor: Colors.blueAccent,
-                      onPressed: () {
-                        setPolyLines( double.parse( _distance.text));
-                        var change =change_in_latitude(10.0);
-                        print(change+myLocation.latitude);
-                        var change_long = change_in_longitude(10.0);
-                        print(change_long+myLocation.longitude);
-                      },
-                      child: Text(
-                        "Find me a route",
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    )
                   ),
-
-                      Container(
-                          margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-
-                          child:
-                          FlatButton(
-                            color: Colors.blue,
-                            textColor: Colors.white,
-                            disabledColor: Colors.grey,
-                            disabledTextColor: Colors.black,
-                            padding: EdgeInsets.all(8.0),
-                            splashColor: Colors.blueAccent,
-                            onPressed: () {
-                              _getLocation();
-                            },
-                            child: Text(
-                              "Find Me",
-                              style: TextStyle(fontSize: 20.0),
-                            ),
-                          )
-                      )
-              ])
-              )
-            ],
-          )
-      ),
+        ])
+        )
     );
   }
 }
@@ -305,91 +326,29 @@ class MyMapState extends State<MapSample> {
 
 
 
-//
-//import 'package:flutter/material.dart';
-//import 'package:google_map_polyline/google_map_polyline.dart';
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
-//import 'package:permission/permission.dart';
-//
-//void main() => runApp(MyApp());
-//
-//class MyApp extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      debugShowCheckedModeBanner: false,
-//      home: MyHomePage(),
-//    );
-//  }
-//}
-//
-//class MyHomePage extends StatefulWidget {
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  final Set<Polyline> polyline = {};
-//
-//  GoogleMapController _controller;
-//  List<LatLng> routeCoords;
-//  GoogleMapPolyline googleMapPolyline =
-//  new GoogleMapPolyline(apiKey: "AIzaSyDFphgQHYwYhst9ZGNfkct-5ZAxF6GWQdI");
-//
-//  getsomePoints() async {
-//    var permissions =
-//    await Permission.getPermissionsStatus([PermissionName.Location]);
-//    if (permissions[0].permissionStatus == PermissionStatus.notAgain) {
-//      var askpermissions = await Permission.requestPermissions([PermissionName.Location]);
-//    } else {
-//      routeCoords = await googleMapPolyline.getCoordinatesWithLocation(
-//          origin: LatLng(40.6782, -73.9442),
-//          destination: LatLng(40.6944, -73.9212),
-//          mode: RouteMode.driving);
-//    }
-//  }
-//
-//  getaddressPoints() async {
-//    routeCoords = await googleMapPolyline.getPolylineCoordinatesWithAddress(
-//        origin: '55 Kingston Ave, Brooklyn, NY 11213, USA',
-//        destination: '178 Broadway, Brooklyn, NY 11211, USA',
-//        mode: RouteMode.driving);
-//  }
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//    getaddressPoints();
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//        body:
-//
-//
-//         GoogleMap(
-//          onMapCreated: onMapCreated,
-//          polylines: polyline,
-//          initialCameraPosition:
-//          CameraPosition(target: LatLng(40.6782, -73.9442), zoom: 14.0),
-//          mapType: MapType.normal,
-//        ));
-//  }
-//
-//  void onMapCreated(GoogleMapController controller) {
-//    setState(() {
-//      _controller = controller;
-//      print(routeCoords);
-//      polyline.add(Polyline(
-//          polylineId: PolylineId('route1'),
-//          visible: true,
-//          points: routeCoords,
-//          width: 4,
-//          color: Colors.blue,
-//          startCap: Cap.roundCap,
-//          endCap: Cap.buttCap));
-//    });
-//  }
-//}
+
+
+class TextBox extends StatelessWidget {
+  TextEditingController _text;
+
+  TextBox(TextEditingController distance){
+    _text=distance;
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      color: Colors.white,
+      child: TextField(
+        controller: _text,
+        decoration:
+        InputDecoration(border: InputBorder.none, hintText: 'Distance'),
+      ),
+    );
+  }
+}
+
+
